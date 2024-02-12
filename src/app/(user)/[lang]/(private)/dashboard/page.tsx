b3db1e59a5172b773077661f0edb9661c41d0590
k0kishima <56298669+k0kishima@users.noreply.github.com>
2024-02-12 15:14:45 +0900

    Implement a method which selects words randomly

--- src/app/(user)/[lang]/(private)/dashboard/page.tsx
+++ src/app/(user)/[lang]/(private)/dashboard/page.tsx
@@ -1,8 +1,17 @@
-export default function Page() {
+import { getRandomWords } from '@/db/word';
+import { Word } from '@prisma/client';
+
+export default async function Page() {
+  const words = await getRandomWords(10);
+
   return (
     <div>
-      <h1>Dashboard</h1>
-      <p>This is the dashboard page.</p>
+      <h1>Random Words</h1>
+      <ul>
+        {words.map((word: Word) => (
+          <li key={word.id}>{word.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
