1a7f90c414f8fbd2677b4daf6f28ed46200cf3de
k0kishima <56298669+k0kishima@users.noreply.github.com>
2024-02-13 20:57:40 +0900

    Add a action for start examination form

--- src/app/(user)/[lang]/(private)/_actions/index.ts
+++ src/app/(user)/[lang]/(private)/_actions/index.ts
@@ -1,6 +1,9 @@
 'use server';
 
 import { signOut as authSignOut } from '@/auth';
+import { auth } from '@/auth';
+import { getUserByEmail } from '@/db/user';
+import { createExamination } from '@/db/examination';
 
 export type ActionsResult =
   | {
@@ -31,4 +34,37 @@ export const signOut = async (): Promise<ActionsResult> => {
       },
     };
   }
+};
+
+export const startExamination = async (
+  wordQuantity = 10,
+): Promise<ActionsResult> => {
+  const sessionUser = await auth().then((session) => session?.user);
+  if (!sessionUser?.email) {
+    return {
+      isSuccess: false,
+      message: {
+        error: 'Could not obtain credentials.',
+      },
+    };
+  }
+  const user = await getUserByEmail(sessionUser.email);
+  if (!user) {
+    return {
+      isSuccess: false,
+      message: {
+        error: 'User not found.',
+      },
+    };
+  }
+  const examination = await createExamination(user.id);
+
+  console.log(examination);
+
+  return {
+    isSuccess: true,
+    message: {
+      info: 'Examination started',
+    },
+  };
 };
--- src/app/(user)/[lang]/(private)/dashboard/_components/buttons.tsx
+++ src/app/(user)/[lang]/(private)/dashboard/_components/buttons.tsx
@@ -2,29 +2,25 @@
 
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { buttonVariants } from '@/components/ui/buttons';
+import { startExamination } from '../../_actions';
+
+const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  e.preventDefault();
+  const isConfirmed = window.confirm('Would you like to start the test?');
+  if (isConfirmed) {
+    await startExamination();
+  }
+};
 
 export function StartExaminationButton() {
   const buttonClassName = buttonVariants();
 
-  const confirmStartExamination = () => {
-    const isConfirmed = window.confirm('試験を開始しますか？');
-    if (isConfirmed) {
-      startExamination();
-    }
-  };
-
-  const startExamination = () => {
-    console.log('試験を開始します');
-  };
-
   return (
-    <button
-      onClick={confirmStartExamination}
-      className={`${buttonClassName} w-full`}
-      type="button"
-    >
-      Start Examination
-      <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
-    </button>
+    <form onSubmit={onSubmit}>
+      <button className={`${buttonClassName} w-full`} type="submit">
+        Start Examination
+        <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+      </button>
+    </form>
   );
 }
