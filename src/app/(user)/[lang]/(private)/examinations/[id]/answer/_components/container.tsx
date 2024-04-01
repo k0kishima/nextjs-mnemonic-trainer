2f3cb9d2d43bd0669c0518374b3eab05af9bac83
k0kishima <56298669+k0kishima@users.noreply.github.com>
2024-04-01 20:11:46 +0900

    Fix submit buttons to consider Call To Action

--- src/app/(user)/[lang]/(private)/examinations/[id]/answer/_components/container.tsx
+++ src/app/(user)/[lang]/(private)/examinations/[id]/answer/_components/container.tsx
@@ -2,16 +2,12 @@ import { auth } from '@/auth';
 import { getExamination } from '@/db/examination';
 import { t } from 'i18next';
 import { Form } from './form';
-import { ArrowRightIcon } from '@heroicons/react/20/solid';
-import { buttonVariants } from '@/components/ui/buttons';
 import { redirect } from 'next/navigation';
 
 /**
  * @package
  */
 export async function Container({ examinationId }: { examinationId: string }) {
-  const buttonClassName = buttonVariants();
-
   const sessionUser = await auth().then((session) => session?.user);
   if (sessionUser == null) {
     throw new Error('No user found');
@@ -35,16 +31,6 @@ export async function Container({ examinationId }: { examinationId: string }) {
         <h1>Please Submit Answer</h1>
         <Form examinationId={examinationId} />
       </div>
-      <footer className="fixed inset-x-0 bottom-0 border-t p-4">
-        <button
-          className={`${buttonClassName} w-full`}
-          type="submit"
-          form="answerForm"
-        >
-          Submit
-          <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
-        </button>
-      </footer>
     </div>
   );
 }
